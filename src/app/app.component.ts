734592fc564bc172bbff4c22fe06246f1f98ac5e
Paul Spears <paul.spears@oasisdigital.com>
2018-02-20 23:47:39 -0600

    curriculum update

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+﻿import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
+
+import { PickBerryAction, AppState, PickAppleAction, PickApplesAction, EmptyCartAction } from './state';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  berry: Observable<number>;
+  apple: Observable<number>;
+  total: Observable<number>;
+
+  constructor(public store: Store<AppState>) {
+    this.berry = store.select(myAppState => myAppState.berryCounter);
+    this.apple = store.select(state => state.appleCounter);
+    this.total = store.pipe(map(s => s.berryCounter + s.appleCounter));
+  }
+
+  pickBerry() {
+    this.store.dispatch(new PickBerryAction());
+  }
+
+  pickApple() {
+    this.store.dispatch(new PickAppleAction());
+  }
+
+  pickApples() {
+    this.store.dispatch(new PickApplesAction(3));
+  }
+
+  empty() {
+    this.store.dispatch(new EmptyCartAction());
+  }
+}
